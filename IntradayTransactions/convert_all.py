4636b366463942fb35833dae84b413afb8f9dc48
stkx2 <stkx2g@gmail.com>
2012-12-17 20:13:38 +0800

    add batch convert: convert_all.py
    Usage:
    convert_all <folder>

---
+++ IntradayTransactions/convert_all.py
@@ -0,0 +1,21 @@
+﻿# 
+import os
+import sys
+import fnmatch
+import re
+
+# find xls data
+source = os.getcwd()
+if (len(sys.argv)==2):
+    source = sys.argv[1]
+
+xlsfile = fnmatch.translate('*.xls')
+for filename in os.listdir(source):
+    if re.match(xlsfile, filename):
+        filename = os.path.join(source, filename)
+        filename2 = filename + '.sql.txt'
+        print filename
+        run1 = 'convert "%s" "convert_template_am5min.txt" >> "%s"' % (filename, filename2)
+        os.system(run1)
+        run2 = 'convert "%s" "convert_template_pm5min.txt" >> "%s"' % (filename, filename2)
+        os.system(run2)
