fb8df3caff3bfb8fd2364695d3e02e1dab9d2661
Joe Huang <joe.q.huang@gmail.com>
2012-12-23 00:46:47 +0800

    fixTimeFormat.py
    1. fixTimeFormat.py
    1) use 24 hour format
    2) remove ';' at the end of each line
    2. loadSymbols.sql
    use '\r\n' as the line terminator for 'load data infile' sql statement

---
+++ IntradayTransactions/fixTimeFormatIssue.py
@@ -0,0 +1,32 @@
+﻿import os
+import sys
+
+def removeNewLine(linesFromFile):
+    newLines = []
+    for line in linesFromFile:
+        newLines.append(line.rstrip("\n"))
+    return newLines
+
+if (len(sys.argv)==3): 
+    g_dir1 = os.path.abspath(sys.argv[1])
+    g_dir2 = os.path.abspath(sys.argv[2])
+    if not g_dir1.upper() == g_dir2.upper():
+        if os.path.exists(g_dir1):
+            if not os.path.exists(g_dir2):
+                os.mkdir(g_dir2)
+            for root, dirs, files in os.walk(g_dir1):
+                for filename in files:
+                    print('Fix ' + filename + ' ... ')
+                    f_old = open(os.path.join(g_dir1,filename))
+                    lines = f_old.readlines()
+                    f_old.close()
+                    f_new = open(os.path.join(g_dir2,filename),'wt')
+                    for line in lines:
+                        line2 = line.replace(';','')
+                        line2 = line2.replace(' 12:59',' 13:00')
+                        line2 = line2.replace(' 01:',' 13:')
+                        line2 = line2.replace(' 02:',' 14:')
+                        f_new.write(line2)
+                    f_new.close()
+else: 
+    print 'Usage: convert <old_folder> <new_folder>' 
