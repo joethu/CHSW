a379475ef60ab4686a38283b33b81956e3e52d2b
Joe Huang <joe.q.huang@gmail.com>
2012-12-29 19:10:38 +0800

    First submit for Strategy

---
+++ Strategies/FSMA.py
@@ -0,0 +1,109 @@
+﻿import StrategyUtils
+
+g_FSMAConfigFastMA = 'fast_ma'
+g_FSMAConfigSlowMA = 'slow_ma'
+g_FSMAConfigCSMD = 'check_slowma_direction'
+
+DEBUG = True
+ALLOW_SHORT = True
+
+def FSMA_Init():
+    paramList = [g_FSMAConfigFastMA,g_FSMAConfigSlowMA,g_FSMAConfigCSMD]
+    return StrategyUtils.readStrategySettings('FSMA',paramList)
+
+def FSMA_CalcDirection(lastPx,currPx,lastDirection):
+    diff = currPx - lastPx
+    dir = lastDirection
+    if diff > 0:
+        dir = 1
+    elif diff < 0:
+        dir = -1
+    return dir
+
+def FSMA(data_in, params):
+    # calculate moving averages
+    slow_ma = int(params[g_FSMAConfigSlowMA])
+    fast_ma = int(params[g_FSMAConfigFastMA])
+    check_direction = bool(int(params[g_FSMAConfigCSMD]))
+    if slow_ma <= fast_ma:
+        print ('ERROR Setting: Slow avg range must be larger than Fast!')
+        return []
+    timePriceList = data_in['data']
+    symbol = data_in['symbol']
+    print("Running FastSlowMovingAverageLineStrategy on symbol " + symbol)
+
+    slow_ma_list = StrategyUtils.PriceProcess.calcMovingAvg(timePriceList,slow_ma)
+    fast_ma_list = StrategyUtils.PriceProcess.calcMovingAvg(timePriceList,fast_ma)
+    if DEBUG:
+        StrategyUtils.TimePriceInfo.printTimePriceSequence(timePriceList,symbol + '.orig.txt')
+        StrategyUtils.TimePriceInfo.printTimePriceSequence(slow_ma_list,symbol + '.avg' + str(slow_ma) + '.txt')
+        StrategyUtils.TimePriceInfo.printTimePriceSequence(fast_ma_list,symbol + '.avg' + str(fast_ma) + '.txt')
+
+    tradeSignals = []
+    position = StrategyUtils.TradeMeta.POS_NONE
+    lastDiff = 0 #slow - fast
+    lastSlow = lastFast = -1 #calc moving direction
+    dirSlow = dirFast = 0    #moving direction: +1, 0, -1
+    
+    for i in range(len(timePriceList)):
+        slow_avg = slow_ma_list[i].price
+        fast_avg = fast_ma_list[i].price
+        if slow_avg > 0 and fast_avg > 0:
+            currDiff = fast_avg - slow_avg
+            if lastSlow > 0: # ignore the first avg pair
+                # update moving direction
+                dirSlow = FSMA_CalcDirection(lastSlow,slow_avg,dirSlow)
+                dirFast = FSMA_CalcDirection(lastFast,fast_avg,dirFast)
+                if currDiff > 0:
+                    if lastDiff < 0:
+                        if (not position==StrategyUtils.TradeMeta.POS_LONG) and \
+                           ((not check_direction) or (dirSlow > 0) or (position==StrategyUtils.TradeMeta.POS_SHORT)):
+                            tradeSignals.append(StrategyUtils.TradeSignal(timePriceList[i].timeStamp,StrategyUtils.TradeMeta.SIDE_BUY,1))
+                            if position==StrategyUtils.TradeMeta.POS_SHORT:
+                                position = StrategyUtils.TradeMeta.POS_NONE
+                            else:
+                                position = StrategyUtils.TradeMeta.POS_LONG
+                    lastDiff = currDiff
+                elif currDiff < 0:
+                    if lastDiff > 0:
+                        if ((ALLOW_SHORT and not position==StrategyUtils.TradeMeta.POS_SHORT) or ((not ALLOW_SHORT) and position==StrategyUtils.TradeMeta.POS_LONG)) and \
+                           ((not check_direction) or (dirSlow < 0) or (position==StrategyUtils.TradeMeta.POS_LONG)):
+                            tradeSignals.append(StrategyUtils.TradeSignal(timePriceList[i].timeStamp,StrategyUtils.TradeMeta.SIDE_SELL,1))
+                            if position==StrategyUtils.TradeMeta.POS_LONG:
+                                position = StrategyUtils.TradeMeta.POS_NONE
+                            else:
+                                position = StrategyUtils.TradeMeta.POS_SHORT
+
+                    lastDiff = currDiff
+                # don't update lastDiff if currDiff = 0
+            lastSlow = slow_avg
+            lastFast = fast_avg
+    return tradeSignals
+
+def FSMA_run():
+    # read strategy config
+    strategy_params = FSMA_Init()
+    # fetch data from db
+    symbol = 'sz000001'
+    pxType = 'ClosePx'
+    start = '2011-01-01'
+    end = '2012-01-01'
+    timePriceList = StrategyUtils.queryPriceFromDB('chswdata','ohlcquotation',symbol,pxType,start,end)
+    print (str(len(timePriceList)) + ' rows retrieved.')
+    # go
+    data_in_dict = dict()
+    data_in_dict['symbol'] = symbol
+    data_in_dict['pxType'] = pxType
+    data_in_dict['data'] = timePriceList
+    data_in_dict['start'] = start
+    data_in_dict['end'] = end
+    tradeSignals = FSMA(data_in_dict,strategy_params)
+    if DEBUG:
+        StrategyUtils.TradeSignal.printTradeSignalSequence(tradeSignals,symbol + '.signal.txt')
+    #evaluate
+    positionLists = StrategyUtils.TradeSignal.computePositionsWithSignals(timePriceList,tradeSignals)
+    if DEBUG:
+        StrategyUtils.PositionInfo.printPositionSequence(positionLists,symbol + '.position.txt')
+
+
+FSMA_run()
---
+++ Strategies/StrategyUtils.py
@@ -0,0 +1,180 @@
+﻿import ConfigParser
+import MySQLdb
+
+CONFIG_FILE = 'Strategy.ini'
+DB_SERVER = 'chsw1982.oicp.net'
+DB_PORT = 3306
+DB_USER = 'root'
+DB_PASSWD = 'root'
+DB_MAX_ROWS = 100000
+def stripNewLine(linesFromFile):
+    newLines = []
+    for line in linesFromFile:
+        newLines.append(line.rstrip("\n"))
+    return newLines
+
+def readStrategySettings(strategy,paramList):
+    config = ConfigParser.RawConfigParser(allow_no_value=True)
+    config.readfp(open(CONFIG_FILE))
+    params = dict()
+    if config.has_section(strategy):
+        for param in paramList:
+            params[param] = config.get(strategy,param)
+    return params
+
+class TradeMeta:
+    POS_LONG = 'L'
+    POS_SHORT = 'S'
+    POS_NONE = 'N'
+    SIDE_BUY = 'B'
+    SIDE_SELL = 'S'
+    SIDE_NONE = 'N'
+
+class TimePriceInfo:
+    def __init__(self,_timeStamp,_price):
+        self.timeStamp = _timeStamp
+        self.price = _price
+
+    @staticmethod
+    def printTimePriceSequence(_timePriceList,_fileName):
+        file = open(_fileName,'wb')
+        for item in _timePriceList:
+            file.write(str(item.timeStamp) + '\t' + str(item.price) + '\n')
+        file.close()
+
+class PositionInfo:
+    def __init__(self,_timeStamp,_capital,_posType):
+        self.timeStamp = _timeStamp
+        self.capital = _capital
+        self.posType = _posType
+
+    @staticmethod
+    def printPositionSequence(_positionList,_fileName):
+        file = open(_fileName,'wb')
+        for item in _positionList:
+            file.write(str(item.timeStamp) + '\t' + str(item.capital) + '\t' + item.posType + '\n')
+        file.close()
+
+class TradeSignal:
+    def __init__(self,_timeStamp,_side,_intensity):
+        self.timeStamp = _timeStamp
+        self.side = _side
+        self.intensity = _intensity # (0,1]
+
+    @staticmethod
+    def printTradeSignalSequence(_tradeSignals,_fileName):
+        file = open(_fileName,'wb')
+        for item in _tradeSignals:
+            file.write(str(item.timeStamp) + '\t' + item.side + '\t' + str(item.intensity) + '\n')
+        file.close()
+
+    @staticmethod
+    def computePositionsWithSignalsHelper(_timePriceList,_signal,_indexOfSignal,_lastIndexOfSignal,_lastPos,_lastCapital,_lastPrice,_positionList):
+        #update positions till this signal
+        capital = _lastCapital
+        price = _lastPrice
+        for index in range(_lastIndexOfSignal+1,_indexOfSignal+1):
+            timePriceItem = _timePriceList[index]
+            price = timePriceItem.price
+            if _lastPos==TradeMeta.POS_LONG:
+                capital = _lastCapital * (_signal.intensity * price / _lastPrice + (1 - _signal.intensity))
+            elif _lastPos==TradeMeta.POS_SHORT:
+                capital = _lastCapital * (_signal.intensity * _lastPrice / price + (1 - _signal.intensity))
+            _positionList.append(PositionInfo(timePriceItem.timeStamp,capital,_lastPos))
+        #trade on this signal
+        pos = _lastPos
+        if _signal.side==TradeMeta.SIDE_BUY:
+            if _lastPos==TradeMeta.POS_SHORT:
+                pos = TradeMeta.POS_NONE
+            elif _lastPos==TradeMeta.POS_NONE:
+                pos = TradeMeta.POS_LONG
+            else:
+                print ("Error - incorrect signal: continuous buying?")
+        else:
+            if _lastPos==TradeMeta.POS_LONG:
+                pos = TradeMeta.POS_NONE
+            elif _lastPos==TradeMeta.POS_NONE:
+                pos = TradeMeta.POS_SHORT
+            else:
+                print ("Error - incorrect signal: continuous selling?")
+        return (_indexOfSignal,pos,capital,price)
+        
+        
+        
+    @staticmethod
+    def computePositionsWithSignals(_timePriceList,_tradeSignals):
+        #simply assume timestamps in _tradeSignals is within those in _timePriceList
+        timeStamps = []
+        for item in _timePriceList:
+            timeStamps.append(item.timeStamp)
+        positionList = []
+        length = len(timeStamps)
+        if length > 0:
+            lastCapital = 1
+            lastPos = TradeMeta.POS_NONE
+            lastIndexOfSignal = 0
+            lastPrice = _timePriceList[0].price
+            positionList.append(PositionInfo(timeStamps[0],lastCapital,lastPos))
+            for sig in _tradeSignals:
+                try:
+                    indexOfSignal = timeStamps.index(sig.timeStamp,lastIndexOfSignal)
+                except:
+                    print ("Error - signal timestamp out of range: " + sig.timeStamp)
+                    indexOfSignal = length - 1
+                    break
+                (lastIndexOfSignal,lastPos,lastCapital,lastPrice) = TradeSignal.computePositionsWithSignalsHelper(_timePriceList,sig,indexOfSignal,lastIndexOfSignal,lastPos,lastCapital,lastPrice,positionList)
+            if lastIndexOfSignal < length - 1:
+                #fake a last signal
+                indexOfSignal = length - 1
+                fakeSignal = TradeSignal(timeStamps[indexOfSignal],TradeMeta.SIDE_NONE,1)
+                TradeSignal.computePositionsWithSignalsHelper(_timePriceList,fakeSignal,indexOfSignal,lastIndexOfSignal,lastPos,lastCapital,lastPrice,positionList)
+        return positionList
+        
+
+class PriceProcess:
+    @staticmethod
+    def calcMovingAvg(_timePriceList,avgRange=5):
+        res = []
+        if avgRange < 1:
+            avgRange = 5
+        elif avgRange == 1:
+            res = _timePriceList
+        else:
+            length = len(_timePriceList)
+            sum = 0;
+            indexFirstAvg = avgRange - 1
+            # set as -1 when list is shorter than the avgRange
+            for i in range(min(indexFirstAvg,length)):
+                res.append(TimePriceInfo(_timePriceList[i].timeStamp,-1.0))
+                sum = sum + _timePriceList[i].price
+            # calculate the 1st avg
+            if indexFirstAvg < length:
+                sum = sum + _timePriceList[indexFirstAvg].price
+                avg = sum / avgRange
+                res.append(TimePriceInfo(_timePriceList[indexFirstAvg].timeStamp,avg))
+                left = 0
+                # calculate all avg following (left,right]
+                for right in range(indexFirstAvg+1,length):
+                    avg = avg + (_timePriceList[right].price - _timePriceList[left].price) / avgRange
+                    left = left + 1
+                    res.append(TimePriceInfo(_timePriceList[right].timeStamp,avg))
+        return res
+  
+def queryPriceFromDB(database,table,symbol,priceType,start,end):
+    try:
+        conn = MySQLdb.connect(host=DB_SERVER,user=DB_USER,passwd=DB_PASSWD,port=DB_PORT)
+        conn.select_db(database)
+        sql = 'select EndTime, ' + priceType + ' from ' + table + ' where symbol = \'' + symbol + '\' and StartTime > \'' + start + '\' and StartTime < \'' + end + '\' limit ' + str(DB_MAX_ROWS)
+        print(sql + ' ...')
+        conn.query(sql)
+        res = conn.store_result()
+        records = res.fetch_row(maxrows=0)
+        conn.close()
+        timePriceList = []
+        for rec in records:
+            timePriceList.append(TimePriceInfo(str(rec[0]),rec[1]))
+        return timePriceList
+    except MySQLdb.Error,e:
+        print ("MySQL Error %d: %s" % (e.args[0], e.args[1]))
+    return []
+
